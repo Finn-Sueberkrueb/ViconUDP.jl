26339a5b3a85461f305c249289c2e21638a48ec7
finn sueberkrueb <git@rpex.de>
2023-06-06 13:34:39 +0200

    added transfomation test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,22 @@ using Vicon
 using Test
 
 @testset "Vicon.jl" begin
-    # Write your tests here.
+    
+    # transformation test
+    vicon_sys = ViconSystem(;port=51001);
+
+    set_world!(vicon_sys, [0; 0; 0], [0 1 0; 1 0 0; 0 0 1]);
+    R_Vicon = [1 0 0; 0 1 0; 0 0 1]
+
+    # Z axis should not rotate
+    x_Vicon = [0; 0; 1]
+    test_item_Vicon = Vicon.ItemStruct(0, 0, "", x_Vicon, R_Vicon)
+    test_item_W = transform_to_world(vicon_sys, test_item_Vicon);
+    @test test_item_W.x_m == x_Vicon
+
+    # X axis should rotate to Y axis
+    x_Vicon = [1; 0; 0]
+    test_item_Vicon = Vicon.ItemStruct(0, 0, "", x_Vicon, R_Vicon)
+    test_item_W = transform_to_world(vicon_sys, test_item_Vicon);
+    @test test_item_W.x_m == [0; 1; 0]
 end
